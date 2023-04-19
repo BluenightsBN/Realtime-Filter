9082ad42b6bfdbce4c33b2fbfd5cd5743bcbf556
BluenightsBN <122196320+BluenightsBN@users.noreply.github.com>
2023-04-19 19:29:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload() {}
+
+
+
+function setup() {
+
+canvas = createCanvas(350,350);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.size(350,350);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw() {}
+
+
+function take_snapshot() {
+
+save("myFilter.png")
+
+}
